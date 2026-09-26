74aaeb17c65cc14caa16d88c24e93bf76b38c3d4
vivictnsantos-tech <vivictnsantos@gmail.com>
2026-09-26 16:47:03 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
    e por exibir notificações locais quando o navegador permitir.
    ============================================================ */
 
-const CACHE_NAME = 'meu-dia-cache-v9';
+const CACHE_NAME = 'meu-dia-cache-v10';
 const APP_SHELL = [
   './',
   './index.html',
